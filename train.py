49cff3221fb2915b06f9495180b7124800d9ec1d
DESKTOP-OFEIAI7\pjh56 <pjh5672.dev@gmail.com>
2022-10-25 00:00:16 +0900

    chore

--- train.py
+++ train.py
@@ -12,6 +12,12 @@ def train(dataloader, model, criterion, optimizer, device):
     optimizer.zero_grad()
 
     for index, minibatch in enumerate(dataloader):
+        ni = index + len(dataloader) * epoch
+        if ni <= nw:
+            xi = [0, nw]
+            for j, x in enumerate(optimizer.param_groups):
+                x['lr'] = np.interp(ni, xi, [0.1 if j == 0 else 0.0, x['initial_lr'] * lf(epoch)])
+    
         images, labels = minibatch[1], minibatch[2]
         predictions = model(images.to(device))
         loss, items = criterion(predictions=predictions, labels=labels)
@@ -37,12 +43,13 @@ if __name__ == "__main__":
     FILE = Path(__file__).resolve()
     ROOT = FILE.parents[0]
 
-    global color_list, class_list, epoch
+    global color_list, class_list, epoch, nw, lf
 
     yaml_path = ROOT / 'data' / 'voc.yaml'
     input_size = 224
     batch_size = 64
-    num_epoches = 200
+    num_epochs = 200
+    warmup_epoch = 3
     device = torch.device('cuda:0')
 
     train_dataset = Dataset(yaml_path=yaml_path, phase='train')
@@ -55,6 +62,8 @@ if __name__ == "__main__":
     val_dataset.load_transformer(transformer=val_transformer)
     val_loader = DataLoader(dataset=val_dataset, collate_fn=Dataset.collate_fn, batch_size=batch_size, shuffle=False, pin_memory=True)
     
+    nw = max(round(warmup_epoch * len(train_loader)), 100)
+    
     class_list = train_dataset.class_list
     num_classes = len(class_list)
     color_list = generate_random_color(num_classes)
@@ -64,10 +73,14 @@ if __name__ == "__main__":
 
     model = YoloModel(input_size=input_size, num_classes=num_classes, device=device, num_boxes=2).to(device)
     criterion = YoloLoss(input_size=input_size, num_classes=num_classes, device=device, lambda_coord=5.0, lambda_noobj=0.5)
-    optimizer = optim.SGD(model.parameters(), lr=0.0001)
+    optimizer = optim.SGD(model.parameters(), lr=0.001)
+    lf = lambda x: (1 - x / num_epochs) * (1.0 - 0.1) + 0.1
+    scheduler = optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lf)
 
-    for epoch in range(1, num_epoches+1):
+    for epoch in range(num_epochs):
         train(dataloader=train_loader, model=model, criterion=criterion, optimizer=optimizer, device=device)
         validate(cocoGt=cocoGt, dataloader=val_loader, model=model, mAP_file_path=mAP_file_path, conf_threshold=0.01, nms_threshold=0.5, class_list=class_list, color_list=color_list, device=device)
+        if (epoch + 1) > warmup_epoch:
+            scheduler.step()
     
     torch.save(model.state_dict(), f'./model_voc.pt')
