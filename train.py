f9026d7f1b65121ce7aebd7962e89314e821ad45
DESKTOP-OFEIAI7\pjh56 <pjh5672.dev@gmail.com>
2022-10-28 20:33:29 +0900

    set:set param of batch size of 32 to 64

--- train.py
+++ train.py
@@ -73,7 +73,7 @@ def parse_args(make_dirs=True):
     parser.add_argument("--exp_name", type=str, required=True, help="Name to log training")
     parser.add_argument("--data", type=str, default="toy.yaml", help="Path to data.yaml")
     parser.add_argument("--img_size", type=int, default=448, help="Model input size")
-    parser.add_argument("--batch_size", type=int, default=32, help="Batch size")
+    parser.add_argument("--batch_size", type=int, default=64, help="Batch size")
     parser.add_argument("--num_epochs", type=int, default=300, help="Number of training epochs")
     parser.add_argument("--warmup_epoch", type=int, default=1, help="Epochs for warming up training")
     parser.add_argument("--init_lr", type=float, default=0.0001, help="Learning rate for inital training")
