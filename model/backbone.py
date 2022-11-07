f2917e5aa8dc11a359d551347067875c3a268b9c
DESKTOP-OFEIAI7\pjh56 <pjh5672.dev@gmail.com>
2022-11-07 23:37:40 +0900

    chore

--- model/backbone.py
+++ model/backbone.py
@@ -39,7 +39,7 @@ class ResNet(nn.Module):
             # so that the residual branch starts with zeros, and each residual block behaves like an identity.
             # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
             for m in self.modules():
-                if isinstance(m, Bottleneck):
+                if isinstance(m, BottleNeck):
                     nn.init.constant_(m.bn3.weight, 0)
                 elif isinstance(m, BasicBlock):
                     nn.init.constant_(m.bn2.weight, 0)
