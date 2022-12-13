56af4d9741199172da14a33057d7f77efb3bb420
DESKTOP-OFEIAI7\pjh56 <pjh5672.dev@gmail.com>
2022-12-13 20:20:03 +0900

    chore

--- model/yolo.py
+++ model/yolo.py
@@ -31,7 +31,7 @@ class YoloModel(nn.Module):
         
         if pretrained:
             ckpt = torch.load(ROOT / "weights" / f"yolov1_{backbone}.pt", map_location="cpu")
-            self.load_state_dict(ckpt["model_state"], strict=False)
+            self.load_state_dict(ckpt["model_state"], strict=True)
 
 
     def forward(self, x):
