4864621dcab9f68e86c0c5762770b8e0c348c841
DESKTOP-OFEIAI7\pjh56 <pjh5672.dev@gmail.com>
2022-12-14 15:24:04 +0900

    update

--- model/element.py
+++ model/element.py
@@ -2,25 +2,32 @@ from torch import nn
 
 
 class Conv(nn.Module):
-    def __init__(self, c1, c2, kernel_size, stride=1, padding=0, dilation=1, act=True, depthwise=False):
+    def __init__(self, c1, c2, kernel_size, stride=1, padding=0, dilation=1, act='leaky_relu', depthwise=False):
         super().__init__()
+        if act == "relu":
+            act_func = nn.ReLU(inplace=True)
+        elif act == "leaky_relu":
+            act_func = nn.LeakyReLU(0.1, inplace=True)
+        elif act == "identity":
+            act_func = nn.Identity()
+
         if depthwise:
             self.conv = nn.Sequential(
                 ### Depth-wise ###
                 nn.Conv2d(c1, c1, kernel_size=kernel_size, stride=stride, padding=padding, dilation=dilation, groups=c1, bias=False),
                 nn.BatchNorm2d(c1),
-                nn.LeakyReLU(0.1, inplace=True) if act else nn.Identity(),
+                act_func,
                 ### Point-wise ###
                 nn.Conv2d(c1, c2, kernel_size=1, stride=stride, padding=0, dilation=dilation, groups=1, bias=False),
                 nn.BatchNorm2d(c2),
-                nn.LeakyReLU(0.1, inplace=True) if act else nn.Identity(),
+                act_func,
             )
         else:
             self.conv = nn.Sequential(
                 ### General Conv ###
                 nn.Conv2d(c1, c2, kernel_size=kernel_size, stride=stride, padding=padding, dilation=dilation, groups=1, bias=False),
                 nn.BatchNorm2d(c2),
-                nn.LeakyReLU(0.1, inplace=True) if act else nn.Identity(),
+                act_func,
             )
 
     def forward(self, x):
