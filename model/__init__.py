4dadd3da759878d98908f42d01d7afc41742cf42
DESKTOP-OFEIAI7\pjh56 <pjh5672.dev@gmail.com>
2022-12-14 09:57:05 +0900

    update yolo

--- model/__init__.py
+++ model/__init__.py
@@ -1,8 +1,7 @@
 import sys
 from pathlib import Path
 
-FILE = Path(__file__).resolve()
-ROOT = FILE.parents[0]
+ROOT = Path(__file__).resolve().parents[0]
 if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))
 
