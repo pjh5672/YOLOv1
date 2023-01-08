f80147c045686d6192cb2023a335cd59fdca6ade
jiho park <pjh5672.dev@gmail.com>
2023-01-08 11:33:06 +0900

    update model auto-download

--- test.py
+++
@@ -1,5 +0,0 @@
-import gdown
-
-url = 'https://drive.google.com/file/d/1EETZU5z4c1lff3zOBk6jHFwBsORd065X/view?usp=share_link'
-output_path = './weights/yolov1-resnet18.pt'
-gdown.download(url, output_path, quiet=False,fuzzy=True)
